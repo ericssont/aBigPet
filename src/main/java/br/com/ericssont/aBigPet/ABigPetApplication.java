463c4f6ba3fa6f9cc7c74d5e744ad4410609f598
Ericsson Tribess <ericktribess@gmail.com>
2023-02-21 19:41:18 -0300

    First commit

---
+++ src/main/java/br/com/ericssont/aBigPet/ABigPetApplication.java
@@ -0,0 +1,13 @@
+package br.com.ericssont.aBigPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ABigPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ABigPetApplication.class, args);
+	}
+
+}
